02b3d141764b3c5a7df62491d2f46cce3906bd4e
kaufma <34608071+kaufma@users.noreply.github.com>
2021-10-31 16:14:15 +0200

    cosmetics

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import React, { useState } from 'react';
 import DataTable from 'react-data-table-component';
 import * as XLSX from 'xlsx';
 
-// https://www.cluemediator.com/read-csv-file-in-react
 function App() {
 
   const [columns, setColumns] = useState([]);
   const [data, setData] = useState([]);
+  const [disable, setDisable] = useState(false);
 
 
-  // process CSV data
   const processData = dataString => {
 
     const dataStringLines = dataString.split(/\r\n|\n/);
     const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
 
-    const list = []; // list of objects, each represent a row in the table.
+    const list = [];
     for (let i = 1; i < dataStringLines.length; i++) {
-      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/); // typeof row = array[string]
-      if (headers && row.length == headers.length) {
+      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+      if (headers && row.length === headers.length) {
         const obj = {};
         for (let j = 0; j < headers.length; j++) {
           let d = row[j];
           if (d.length > 0) {
-            if (d[0] == '"')
+            if (d[0] === '"')
               d = d.substring(1, d.length - 1);
-            if (d[d.length - 1] == '"')
+            if (d[d.length - 1] === '"')
               d = d.substring(d.length - 2, 1);
           }
           if (headers[j]) {
@@ -34,78 +33,66 @@ function App() {
           }
         }
 
-        // remove the blank rows
         if (Object.values(obj).filter(x => x).length > 0) {
           list.push(obj);
         }
       }
     }
 
-    // prepare columns list from headers
     const columns = headers.map(c => ({
       name: c,
-      selector: c,
+      selector: row => row[c],
     }));
 
     setData(list);
     setColumns(columns);
   }
 
-  // handle file upload
+
   const handleFileUpload = e => {
-    console.log(e.target)
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.onload = (evt) => {
-      /* Parse data */
       const bstr = evt.target.result;
       const wb = XLSX.read(bstr, { type: 'binary' });
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0]; //workbook.SheetNames is an ordered list of the sheets in the workbook
-      const ws = wb.Sheets[wsname]; //wb.Sheets[sheetname] returns an object representing the worksheet.
-      /* Convert array of arrays */
-      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 }); //sheet_to_csv generates delimiter-separated-values output.
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
       processData(data);
     };
     reader.readAsBinaryString(file);
   }
 
-  const AddSchoolCol = () => {
+  const addSchoolCol = () => {
+    setDisable(true)
     setColumns([
       ...columns,
       {
         name: "Schools",
-        selector: "Schools"
+        selector: row => row["Schools"]
       }
     ]);
 
-    const promises = []
-
     for (let i = 0; i < data.length; i++) {
-      FetchData(data[i])
-        .then(newHouseObj => {
-          setData([...data], data[i].Schools = newHouseObj.Schools)
-          console.log(`id = ${data[i].SAMPLE_ID} newHouseObj.Schools= ${newHouseObj.Schools}, data[i].Schools = ${data[i].Schools} `)
-        })
-        .catch(error => console.log(`Error in promises ${error}`))
+      (async () => {
+
+        let newHouseObj = await fetchData(data[i])
+        setData([...data], data[i].Schools = newHouseObj.Schools)
+        console.log(`id = ${data[i].SAMPLE_ID} newHouseObj.Schools= ${newHouseObj.Schools}, data[i].Schools = ${data[i].Schools}`)
+      })()
     }
 
-    // data.forEach(houseObj => {
-    //   FetchData(houseObj)
-    //     .then(newHouseObj => {
+    // const promises = []
+    // for (let i = 0; i < 5; i++) { fetchData(data[i]).then(res => promises.push(res)) }
+    // Promise.all(promises).then(res => {
+    //   console.log("promise finished")
+    //   setData([...data], res)
+    // });
 
-    //       console.log(setData([...data, data.filter(obj => obj.SAMPLE_ID === newHouseObj.SAMPLE_ID)[0].school = newHouseObj.school)]) 
 
-    //     })
-    //     .catch(error => console.log(`Error in promises ${error}`))
-    // })
-
-    //   data.forEach(houseObj => { promises.push(FetchData(houseObj)) })
-    //   console.log(promises)
-    //   Promise.all(promises).then(vals => console.log(vals)).catch(error => console.log(`Error in promises ${error}`))
-    // }
   }
-  const FetchData = async (rowObj) => {
+  const fetchData = async (rowObj) => {
 
     const res = await axios.post("http://localhost:8000/api/enrich/", rowObj)
 
@@ -115,13 +102,13 @@ function App() {
 
   return (
     <div>
-      <h3>Read CSV file in React</h3>
+      <h3>Households Info</h3>
       <input
         type="file"
         accept=".csv,.xlsx,.xls"
         onChange={handleFileUpload}
       />
-      <button onClick={AddSchoolCol}>Add Schools</button>
+      <button onClick={addSchoolCol} disabled={disable}>Add Schools</button>
       <DataTable
         pagination
         highlightOnHover
