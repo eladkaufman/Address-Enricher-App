52d650e65b7cd7a7e28cbef89bda4adf191d02b1
kaufma <34608071+kaufma@users.noreply.github.com>
2021-11-01 16:01:19 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
 import './App.css'
 import React, { useState } from 'react';
-import * as XLSX from 'xlsx';
 import * as ReactBootStrap from 'react-bootstrap';
+import Table from './components/table'
+import handleUpload from './utils/handle-upload';
+import loadBatch from './utils/load-batch';
 
 const BATCH_SIZE = 4
 
@@ -16,58 +17,8 @@ function App() {
   const [loadMoreEnable, setLoadMoreEnable] = useState(false)
   const [currBatchStart, setCurrBatchstart] = useState(0)
 
-  const processData = dataString => {
-
-    const dataStringLines = dataString.split(/\r\n|\n/);
-    const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
-    const list = [];
-    for (let i = 1; i < dataStringLines.length; i++) {
-      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
-      if (headers && row.length === headers.length) {
-        const obj = {};
-        for (let j = 0; j < headers.length; j++) {
-          let d = row[j];
-          if (d.length > 0) {
-            if (d[0] === '"')
-              d = d.substring(1, d.length - 1);
-            if (d[d.length - 1] === '"')
-              d = d.substring(d.length - 2, 1);
-          }
-          if (headers[j]) {
-            obj[headers[j]] = d;
-          }
-        }
-
-        if (Object.values(obj).filter(x => x).length > 0) {
-          list.push(obj);
-        }
-      }
-    }
-    const columns = headers.map(c => ({
-      name: c,
-      selector: row => row[c],
-    }));
-    setData(list);
-    setBatchDataDisplay(list.slice(0, BATCH_SIZE))
-    setColumns(columns);
-
-  }
-
   const handleFileUpload = e => {
-
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      const bstr = evt.target.result;
-      const wb = XLSX.read(bstr, { type: 'binary' });
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-
-      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-      processData(data);
-    };
-    reader.readAsBinaryString(file);
-    setDisable(false)
+    handleUpload({ e, setDisable, setData, setBatchDataDisplay, setColumns, BATCH_SIZE })
   }
 
   const addSchoolCol = () => {
@@ -80,43 +31,11 @@ function App() {
         selector: row => row["Schools"]
       }
     ]);
-    loadBatch()
+    loadNextBatch()
   }
 
-
-  const loadBatch = () => {
-
-    setLoading(true)
-    setLoadMoreEnable(false)
-    const promises = []
-    for (let i = currBatchStart; i < currBatchStart + BATCH_SIZE && i < data.length; i++) { promises.push(fetchData(data[i])) }
-    Promise.all(promises)
-      .then(res => {
-
-        const updatedData = data;
-        res.forEach(houseObj => {
-          if (houseObj !== "") {
-            updatedData[updatedData.findIndex(obj => obj.SAMPLE_ID === houseObj.SAMPLE_ID)].Schools = houseObj.Schools;
-          }
-        })
-
-        setData([...updatedData])
-        setBatchDataDisplay(updatedData.splice(0, currBatchStart + BATCH_SIZE))
-        setLoading(false)
-        setCurrBatchstart(currBatchStart + BATCH_SIZE)
-        if (currBatchStart + BATCH_SIZE >= data.length) {
-          setLoadMoreEnable(false)
-        }
-        setLoadMoreEnable(true)
-      })
-      .catch(err => console.log(err))
-  }
-
-
-  const fetchData = async (rowObj) => {
-
-    const res = await axios.post("http://localhost:8000/api/enrich/", rowObj)
-    return res.data
+  const loadNextBatch = () => {
+    loadBatch({ setLoading, setLoadMoreEnable, currBatchStart, BATCH_SIZE, data, setData, setBatchDataDisplay, setCurrBatchstart })
   }
 
 
@@ -128,37 +47,23 @@ function App() {
         accept=".csv,.xlsx,.xls"
         onChange={handleFileUpload}
       />
-      <button onClick={addSchoolCol} disabled={disable}>Add Schools</button>
+      <button
+        onClick={addSchoolCol}
+        disabled={disable}>
+        Add Schools
+      </button>
+
       {loading && (<ReactBootStrap.Spinner animation="border" />)}
 
-      <table border="1" style={{ margin: "3px" }}>
-        <thead>
-          <tr>
-            {columns.map((col, idx) => {
-              return (
-                <th key={col + idx}>{col.name}</th>
-              )
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {batchDataDisplay.map((houseObj, rowIdx) => {
-            return (
-              <tr key={houseObj + rowIdx}>
-                {Object.values(houseObj).map((value, idx) => {
-                  return (
-                    <td key={value + idx} >{value}</td>
-                  )
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <Table
+        columns={columns}
+        batchDataDisplay={batchDataDisplay} />
 
-
-
-      <button onClick={loadBatch} disabled={!loadMoreEnable}>Load More</button>
+      <button
+        onClick={loadNextBatch}
+        disabled={!loadMoreEnable}>
+        Load More
+      </button>
     </div >
   );
 }
---
+++ src/components/table.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Table = (props) => {
+    const { columns, batchDataDisplay } = props
+    return (
+        <table border="1" style={{ margin: "3px" }}>
+            <thead>
+                <tr>
+                    {columns.map((col, idx) => {
+                        return (
+                            <th key={col + idx}>{col.name}</th>
+                        )
+                    })}
+                </tr>
+            </thead>
+            <tbody>
+                {batchDataDisplay.map((houseObj) => {
+                    return (
+                        <tr key={houseObj.SAMPLE_ID}>
+                            {Object.values(houseObj).map((value, idx) => {
+                                return (
+                                    <td key={value + idx} >{value}</td>
+                                )
+                            })}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    );
+}
+
+export default Table;
